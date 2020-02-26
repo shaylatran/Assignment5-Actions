4590b32697df8e000b36739541baafb2e7a5701e
Shayla Tran <transhay@oregonstate.edu>
2020-02-26 12:15:16 -0800

    Adding task.py and tests.py

---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+import task
+
+class TestCase(unittest.TestCase):
+
+    def test1(self):
+        expected = "success"
+        self.assertEqual(expected, task.firstrun())
+
+    def test2(self):
+        expected = "failure"
+        self.assertNotEqual(expected, task.firstrun())
